dc73d582c058b993f0e3729676fc5669134a60d5
Alexander Babtshe <babtshe@yandex.com>
2018-03-23 19:11:52 +0300

    price-selector js done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,110 @@
 //double-range==============================================================================
-/*
-var handles = document.querySelectorAll('.slider-handle');
-for (var i = 0; i < handles.length; i++) {
-  handles[i].style.background = 'blue';
-  handles[i].addEventListener('mousedown', mouseDown, false);
-  handles[i].addEventListener('mouseup', mouseUp, false);
+//все базовые значения вводятся в html
+//можно допилить ещё управление с клавиатуры
+//и проверку введённых значений.
+try {
+  var isDown = false; // нажата ли мышка
+  var offset = 0;
+  var handle = null;
+  var valueFrom = document.querySelector('.double-range .min-start').innerHTML;
+  var valueTo = document.querySelector('.double-range .max-end').innerHTML;
+  var handles = document.querySelectorAll('.slider-handle');
+  var handleFrom = document.querySelector('.slider-handle.price-from');
+  var handleTo = document.querySelector('.slider-handle.price-to');
+  var sliderRange = document.querySelector('.slider-range');
+} catch (e) {
+  console.log('селектор цены не нашли на страничке');
+}
+if (handleFrom && handleTo) { //отслеживаем клики по ручкам
+  handleFrom.addEventListener('mousedown', mouseDown, false);
+  handleFrom.addEventListener('click', function (event) { //ссылки реагируют на таб, но не реагируют на клик
+    event.preventDefault();
+  }, false);
+  handleTo.addEventListener('mousedown', mouseDown, false);
+  handleTo.addEventListener('click', function (event) {
+    event.preventDefault();
+  }, false);
+  initialiseRange();
 }
 
-function mouseDown(event) {
-  event.currentTarget.style.background = 'green';
-  event.currentTarget.addEventListener('mousemove', mouseMove, false);
+
+function initialiseRange() { //подставим значения ползунков и фона по умолчанию как в макете
+  if (document.querySelector('.double-range')) { //проверим есть ли блок на странице
+    var firstHandle = calcWidth(document.getElementById('range-start').value, valueTo);
+    var lastHandle = calcWidth(document.getElementById('range-end').value, valueTo);
+    handleFrom.style.left = firstHandle + '%';
+    handleTo.style.left = lastHandle + '%';
+    sliderRange.style.left = firstHandle + '%';
+    sliderRange.style.width = (lastHandle - firstHandle) + '%'; //размер фона
+    document.querySelector('.double-range .value-from').innerHTML = 'от ' + (Math.round(firstHandle * valueTo / 100)); //подписи от и до
+    document.querySelector('.double-range .value-to').innerHTML = 'до ' + (Math.round(lastHandle * valueTo / 100));
+  }
 }
 
-function mouseUp(event) {
-  event.currentTarget.removeEventListener('mousemove', mouseMove, false);
-  event.currentTarget.style.background = 'pink';
+function updateValues(handle) {
+  if (handle == handleFrom) {
+    var val = (Math.round(parseInt(handleFrom.style.left) * valueTo / 100));
+    document.querySelector('.double-range .value-from').innerHTML = 'от ' + val;
+    document.getElementById('range-start').value = val;
+  } else {
+    var val = (Math.round(parseInt(handleTo.style.left) * valueTo / 100));
+    document.querySelector('.double-range .value-to').innerHTML = 'до ' + val;
+    document.getElementById('range-end').value = val;
+  }
 }
 
-function mouseMove(event) {
-  event.currentTarget.style.background = 'black';
-  event.currentTarget.style.left = 50 + '%';
-  var min = 0;
-  console.log(event.currentTarget.pageX);
+function calcWidth(curValue, maxValue) {
+  if (maxValue >= 0) {
+    return Math.round(curValue / maxValue * 100);
+  } else {
+    return 0;
+  }
 }
-*/
+
+function mouseDown(event) { //реагирует только на левую кнопку.
+  if (event.button === 0) {
+    isDown = true;
+    handle = event.currentTarget;
+    offset = event.currentTarget.offsetLeft - event.clientX + 10;
+    document.addEventListener('mouseup', function () {
+      if (event.button === 0) {
+        isDown = false;
+      }
+    }, true)
+  }
+}
+
+document.addEventListener('mousemove', function (event) {
+  if (isDown) {
+    event.preventDefault();
+    var minValue = 0;
+    var maxValue = document.querySelector('.price-slider').clientWidth;
+    var moveDistance = event.clientX + offset;
+    if (moveDistance < minValue) {
+      moveDistance = minValue;
+    } else if (moveDistance > maxValue) {
+      moveDistance = maxValue;
+    }
+    moveDistance = calcWidth(moveDistance, maxValue);
+    if (handle == handleFrom) { //первая ручка
+      if (moveDistance > parseInt(handleTo.style.left)) { //не пускаем за вторую
+        moveDistance = parseInt(handleTo.style.left);
+      }
+      handleFrom.style.left = moveDistance + '%';
+      updateValues(handleFrom);
+      sliderRange.style.left = handleFrom.style.left;
+      sliderRange.style.width = parseInt(handleTo.style.left) - parseInt(handleFrom.style.left) + '%';
+    } else { //вторая ручка
+      if (moveDistance < parseInt(handleFrom.style.left)) { //не пускаем за первую
+        moveDistance = parseInt(handleFrom.style.left);
+      }
+      handleTo.style.left = moveDistance + '%';
+      updateValues(handleTo);
+      sliderRange.style.width = parseInt(handleTo.style.left) - parseInt(handleFrom.style.left) + '%';
+    }
+  }
+}, true);
+
 
 //external slider links================================================================================
 if (location.hash) { //например, если ссылка снаружи
@@ -103,7 +184,7 @@ function validateAll(event) {
 //forms show==============================================================================
 var mapLink = document.querySelector('.map-popup-link');
 var mapOverlay = document.getElementById('popup-map');
-if (mapLink) {//map
+if (mapLink) { //map
   mapLink.addEventListener('click', function (event) {
     mapOverlay.classList.add('show');
     event.preventDefault();
@@ -115,8 +196,8 @@ if (mapLink) {//map
       }
     });
     var formClose = mapOverlay.querySelector('.modal-close-btn');
-    if(formClose) {
-      formClose.addEventListener('click', function(event) {
+    if (formClose) {
+      formClose.addEventListener('click', function (event) {
         mapOverlay.classList.remove('show');
         event.preventDefault();
       });
@@ -125,7 +206,7 @@ if (mapLink) {//map
 }
 var contactLink = document.querySelector('.form-contact-link');
 var formOverlay = document.getElementById('popup-writeus');
-if (contactLink) {//contact
+if (contactLink) { //contact
   contactLink.addEventListener('click', function (event) {
     formOverlay.classList.add('show');
     event.preventDefault();
@@ -137,8 +218,8 @@ if (contactLink) {//contact
       }
     });
     var formClose = formOverlay.querySelector('.modal-close-btn');
-    if(formClose) {
-      formClose.addEventListener('click', function(event) {
+    if (formClose) {
+      formClose.addEventListener('click', function (event) {
         formOverlay.classList.remove('show');
         event.preventDefault();
       });
